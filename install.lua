485df5fbdaba1ad95faf6656afe29575736e2a48
Benoxiid <benoxiid@gmail.com>
2023-05-03 16:01:42 +0200

    test install.lua

---
+++ install.lua
@@ -0,0 +1,36 @@
+mainRepo = "https://github.com/Benoxiid/cts-scripts/raw/main/"
+links = {
+    JSON = "https://github.com/tiye/json-lua/raw/main/JSON.lua"
+}
+
+programs = {
+    quarry = {
+        "gantry",
+        "quarry"
+    },
+    storage = {
+        "JSON",
+        "gantry",
+        "storage"
+    }
+}
+
+function install(program)
+end
+
+function prompt(text, ...)
+    print(text)
+
+    i = 1
+    for k,v in pairs(arg) do
+        print(string.format("%d. %v", i, v))
+        i++
+    end
+
+    choice = io.read("*n")
+    return arg[choice]
+end
+
+function run()
+    name = prompt("What program do you want to install ?", table.unpack(programs))
+end
